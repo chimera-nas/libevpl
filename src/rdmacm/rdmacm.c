e5eb721e2abcb928905883c0357bcbdfb19798f0
Ben Jarvis <ben.jarvis@quantum.com>
2025-01-19 13:20:26 +0000

    Fix a couple build nits

--- src/rdmacm/rdmacm.c
+++ src/rdmacm/rdmacm.c
@@ -10,8 +10,8 @@
 #include <netdb.h>
 #include <unistd.h>
 
-#include "uthash.h"
-#include "utlist.h"
+#include "uthash/uthash.h"
+#include "uthash/utlist.h"
 
 #include "rdmacm/rdmacm.h"
 #include "core/internal.h"
